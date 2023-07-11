b46b2ea02164e41b01dd8c1800d169b83aeedabe
blackcowman <soh990301@gmail.com>
2023-07-11 20:44:42 +0900

    delete기능 추가

--- src/main/java/com/chihyangmai/backend/presentation/controller/FundRequestController.java
+++ src/main/java/com/chihyangmai/backend/presentation/controller/FundRequestController.java
@@ -1,8 +1,20 @@
 package com.chihyangmai.backend.presentation.controller;
 
 
+import com.chihyangmai.backend.application.service.FundRequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class FundRequestController {
+
+    @DeleteMapping("/room")
+    public ResponseEntity<Void> deleteById(@RequestParam Long fundrequestId) {
+        FundRequestService.deleteFundRequest(fundrequestId);
+        return ResponseEntity.ok(null);
+    }
 
 
 }
